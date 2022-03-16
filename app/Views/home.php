1fada2e281e5fee71167bf8842a427c032835a81
oAugust <ocarloxx@gmail.com>
2022-03-16 15:09:32 -0300

    primeira atividade de pw, parte-1

---
+++ app/Views/home.php
@@ -0,0 +1,49 @@
+
+        <!-- inicio do corpo da pagina-->
+        <div class="row">
+            <div class="col">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img src="<?=base_url()?>/img/bmw1.jpg" class="d-block w-100 imagem" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                    <img src="<?=base_url()?>/img/bmw2.jpg" class="d-block w-100 imagem" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                    <img src="<?=base_url()?>/img/bmw3.jpg" class="d-block w-100 imagem" alt="...">
+                    </div>
+                    </div>
+                    <div class="carousel-item">
+                    <img src="<?=base_url()?>/img/bmw4.jpg" class="d-block w-100 imagem" alt="...">
+                    </div>
+                    </div>
+                    <div class="carousel-item">
+                    <img src="<?=base_url()?>/img/bmw5.jpg" class="d-block w-100 imagem" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+                </div>
+            </div>
+        </div>
+</br>
+</br>
+        <div class="row">
+            <div class="col">
+                <h2>  Somos a nova filial da BMW e vamos vender os melhores modelos da marca</h2>
+                <img src="<?=base_url()?>/img/bmlogo.png" alt="..." width="190px" height="190px">
+            </div>
+            <div class="col">
+                <h2>  Nós somos autorizados pela BMW pela venda e eles fornecem os carros</h2>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1IWXyyaj9BFD20ocBnktNCWgCp1hiEZgzog&usqp=CAU" alt="">
+            </div>
+        </div>
+        <!-- fim do corpo da pagina-->
+       
